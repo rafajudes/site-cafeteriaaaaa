0d19c5520f2de1de11ea9b5969b95611033509ec
rafajudes <rafaelajudesdasilva@gmail.com>
2026-02-10 14:05:04 -0300

    Add files via upload

---
+++ menuu.js
@@ -0,0 +1,20 @@
+
+let btnMenu = document.getElementById('btn-menu')
+let menu = document.getElementById('menu-mobile')
+let overlay = document.getElementById('overlay-menu')
+
+
+
+btnMenu.addEventListener('click', ()=>{
+    menu.classList.add('menu-abrir')
+})
+
+menu.addEventListener('click', ()=>{
+    menu.classList.remove('menu-abrir')
+})
+overlay.addEventListener('click', ()=>{
+    menu.classList.remove('menu-abrir')
+})
+
+
+
